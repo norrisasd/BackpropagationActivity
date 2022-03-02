4212eb814f3f0f583b23560699b9e9de236aa974
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2022-03-02 23:24:04 +0800

    Add project files.

---
+++ BackpropagationActivity/CsvData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace BackpropagationActivity
+{
+    enum Genders
+    {
+        Male,
+        Female,
+    }
+
+    class CsvData
+    {
+        public List<HeartDisease> getRecords()
+        {
+            using (var reader = new StreamReader("C:\\Users\\norrisasd\\source\\repos\\BackpropagationActivity\\BackpropagationActivity\\heart_v2.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+                List<HeartDisease> records = csv.GetRecords<HeartDisease>().ToList();
+
+
+                return records;
+            }
+        }
+        public int ParseGender(String g)
+        {
+            Genders result;
+            if (Enum.TryParse(g, out result))
+            {
+                switch (result)
+                {
+                    case Genders.Male:
+                        return 1;
+                }
+            }
+            return 0;
+        }
+    }
+
+    public class HeartDisease
+    {
+        public int age { get; set; }
+        public String sex { get; set; }
+        public int BP { get; set; }
+        public int cholestrol { get; set; }
+        public int heart_disease { get; set; }
+    }
+
+}
---
+++ BackpropagationActivity/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace BackpropagationActivity
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet bp;
+        CsvData csvData;
+        public Form1()
+        {
+            InitializeComponent();
+            csvData = new CsvData();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bp = new NeuralNet(4,3,1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<HeartDisease> data = csvData.getRecords();
+            for (int i = 1; i < 10000; i++)
+            {
+                foreach(var d in data)
+                {
+                    bp.setInputs(0, d.age);
+                    bp.setInputs(1, csvData.ParseGender(d.sex));
+                    bp.setInputs(2, d.BP);
+                    bp.setInputs(3, d.cholestrol);
+                    bp.setDesiredOutput(0, d.heart_disease);
+                    bp.learn();
+                    
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bp.loadWeights(openFileDialog1.FileName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt32(textBox1.Text);
+            int gender = csvData.ParseGender(comboBox1.Text);
+            int bp = Convert.ToInt32(textBox2.Text);
+            int chol = Convert.ToInt32(textBox3.Text);
+
+            this.bp.setInputs(0, age);
+            this.bp.setInputs(1, gender);
+            this.bp.setInputs(2, bp);
+            this.bp.setInputs(3, chol);
+            this.bp.run();
+
+            textBox4.Text = "" + this.bp.getOuputData(0);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bp.saveWeights(saveFileDialog1.FileName);
+        }
+    }
+}
